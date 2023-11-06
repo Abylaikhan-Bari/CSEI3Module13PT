92e34d3def73c88950c1515b63ac2e8e43456b8b
Abylaikhan Bariyev <abylaikhanbariev7@gmail.com>
2023-11-06 09:45:22 +0600

    Add project files.

---
+++ Module13PT/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module13PT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+        //static void example1()
+        //{
+        //    ArrayList list = new ArrayList();
+
+        //    object test = 55;//boxing
+        //    int x = (int)test;//unboxing
+
+
+        //    list.Add(1);
+        //    list.Add(true);
+        //    list.Add("Test");
+
+        //    foreach (object item in list)
+        //    {
+        //        int xx = (int)item;
+        //        Console.WriteLine(xx);
+        //    }
+        //}
+
+        //static void example2()
+        //{
+
+        //    //queue
+        //    Queue qe = new Queue();
+
+        //    Random random = new Random();
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        qe.Enqueue(i);
+        //    }
+        //    //Console.WriteLine("Queue \n");
+        //    //foreach (int item in qe)
+        //    //{
+        //    //    Console.WriteLine(qe.Dequeue());
+        //    //}
+
+        //    Console.WriteLine("Queue \n");
+        //    foreach (var item in qe)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //}
+        //static void example3()
+        //{
+        //    //stack
+        //    Stack st = new Stack();
+        //    st.Push("1");
+        //    st.Push("2");
+        //    st.Push("3");
+        //    st.Push("4");
+
+        //    foreach (var item in st)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //}
+
+        //static void example4()
+        //{
+        //    Dictionary<int, string> dic = new Dictionary<int, string>();
+        //    for (int i = 0;i < 10; i++)
+        //    {
+        //        dic.Add(i, i.ToString());
+        //    }
+        //    if (!dic.ContainsKey(5))
+        //    {
+
+        //    }
+        //}
+
+        static void example5()
+        {
+            //            1.Создать коллекцию List<int>.Вывести на экран позицию и значение элемента, являющегося вторым максимальным значением в коллекции
+            //            a.Удалить все нечетные элементы списка List<int>
+
+            List<int> col1 = new List<int>();
+            Random random = new Random();
+
+        }
+    }
+}
